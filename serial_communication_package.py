d34c2dcce59148c4d7eb98083166fde4c2178c6c
amir1970 <58373961+amir1970@users.noreply.github.com>
2021-12-19 20:38:41 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 2b137b784c622440db826251b84d50e086bbe9d3.

--- serial_communication_package.py
+++ serial_communication_package.py
@@ -1,10 +1,6 @@
 import sys
 import functools
 import struct
-from re import match
-
-
-
 
 class Write_Read_Packet_Cls(object):
     global write_package
@@ -31,23 +27,16 @@ class Write_Read_Packet_Cls(object):
     def write_encapsulate_frame(self, opcode, payload: list):
         print(" payload tx =", payload)
 
-        indices = []
-        for i in range(len(payload)):
-            if payload[i] == 126:
-                indices.append(i)
-        print("!!!!!!!!!!!!!!!!!!!    indices     =     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", indices)
-
         tmp_payload = bytearray(payload)
-        for index_7e in indices:
-            if index_7e == -1:
-                print("Not Found 7e ")
-            else:
-                print("Found 7e at location ", index_7e)
-                tmp_payload.insert(index_7e, 125)  # 7D
-                tmp_payload.insert(index_7e + 1, tmp_payload[index_7e + 1] ^ (1 << 5))  # 7E inverted bit 5
-                del tmp_payload[index_7e + 2:index_7e + 3]  # 7E deleted
+        index_7e: int = tmp_payload.find(126)
 
-        print("tmp_payload = ",''.join('{:02x}'.format(x) for x in tmp_payload))
+        if index_7e == -1:
+            print("Not Found 7e ")
+        else:
+            print("Found 7e at location ", index_7e)
+            tmp_payload.insert(index_7e, 125)  # 7D
+            tmp_payload.insert(index_7e + 1, tmp_payload[index_7e + 1] ^ (1 << 5))  # 7E inverted bit 5
+            del tmp_payload[index_7e + 2:index_7e + 3]  # 7E deleted
 
         packet_length: int = len(tmp_payload) + 2
         print("Write packet_length = ", packet_length)
@@ -70,76 +59,19 @@ class Write_Read_Packet_Cls(object):
         print("calc checksum write lst = ", lst)
         return functools.reduce(lambda x, y: x + y, lst) % 256
 
-
-    def handle_read_opcode_0x3b(self,read_packet_obtained:bytearray,index_num:int,channel_request:int):  # , rx_pack_opcode_0x3b:bytearray):
-        counts = bytes(bin(channel_request).count("1") for x in range(256))  # py2: use bytearray
-        #find_pos:list = self.findPosition(channel_request)
-
-        # find_pos = bin(channel_request)
-        # for index_find_pos in find_pos:
-        #     if (index_find_pos == 1):
-        #         print("Found index_find_pos ", index_find_pos)
-        #     else:
-        #         print("Not Found index_find_pos ")
-
-        #print("%%%  find position:  %%%", find_pos)
-        print( "counts=",counts )
-
-        filter_channel_num:int= int(0xE0)
-        filter_meas:int= int(0x1F_FF_FF)
-        mv=memoryview(read_packet_obtained)
-        print("list of memory view= ", list(mv[0::]))
-        tested_int_mv :int=0
-
-        bytes_extracted = bytes(read_packet_obtained)
-        print("bytes_extracted = ", bytes_extracted)
-
-        channel_selected=[]
+    def handle_read_opcode_0x3b(self,read_packet_btained):  # , rx_pack_opcode_0x3b:bytearray):
+        list_index: list = [5, 9, 13, 17, 21]
         meas_result = []
-        for index_i in range(0,5):
-            int_chan_sel: int = int.from_bytes(read_packet_obtained[3*index_i+5:3*index_i+6], "big")
-            int_chan_sel = (int_chan_sel & filter_channel_num)>>5
-            int_meas: int = int.from_bytes(read_packet_obtained[3*index_i+7:3*index_i+9], "big")
-            channel_selected.append(int_chan_sel)
-            meas_result.append(int_meas)
-        print("channel_selected new = ", channel_selected)
-        print("meas_result after append", meas_result)
-        return meas_result
-
-    # Python3 program to find position of
-    # only set bit in a given number
-
-    # A utility function to check
-    # whether n is power of 2 or
-    # not.
-    # def isPowerOfTwo(self,n):
-    #     return (True if (n > 0 and ((n & (n - 1)) > 0)) else False);
-    #
-    # #def isPowerOfTwo(n):
-    #     #pass
-    #
-    #     # Returns position of the
-    #
-    # # only set bit in 'n'
-    # def findPosition(self,n):
-    #     if (self.isPowerOfTwo(n) == True):
-    #         return -1;
-    #     i = 1;
-    #     pos = 1;
-    #     # Iterate through bits of n
-    #     # till we find a set bit i&n
-    #     # will be non-zero only when
-    #     # 'i' and 'n' have a set bit
-    #     # at same position
-    #     while ((i & n) == 0):
-    #         # Unset current bit and
-    #         # set the next bit in 'i'
-    #         i = i << 1;
-    #        # increment position
-    #         pos += 1;
-    #     return pos;
+        print("meas_result", meas_result)
+        for index_i in list_index:
+            int_val1: int = int.from_bytes(read_packet_btained[index_i:index_i + 3], "big")
+            print("index i is = ", index_i, "struct.pack1 new = ", int_val1)
+            meas_result.append(int_val1)
+            print("meas_result after append", meas_result)
+            return meas_result
 
     def encapsulate_read_frame(self, read_payload: list):
+        # print(" payload rx =", read_payload)
         self.read_package = bytearray(read_payload)
         packet_length: int = len(self.read_package)
         calc_read_checksum: int = self.write_read_calc_checksum(self.read_package[1:packet_length - 2])  # Checksum
@@ -163,13 +95,43 @@ class Write_Read_Packet_Cls(object):
 
         if self.read_package[3] == 0x3B:
             print("3B detected : ", self.read_package)
-            self.handle_read_opcode_0x3b(self.read_package,0x01,0x67)
+            self.handle_read_opcode_0x3b(self.read_package)
 
         for x in self.read_package: print(x)
         print()
         for y in self.read_package: print(hex(y))
         print()
         return self.read_package
+
+    def access_bit(self, data, num):
+        base = int(num // 8)
+        shift = int(num % 8)
+        return (data[base] >> shift) & 0x1
+
+    # def read_channel_and_milivolt(self,rd_packet:bytearray):
+    #     milivolt:int=0
+    #     print("rd_packet[x:x+2]=", rd_packet[0:2])
+    #     self.channel_mask_location = self.rd_packet[0:1] & 0xE0  # bits 7:5 are relevant for channel
+    #     milivolt = self.rd_packet[0:3] & 0x1F_FF_FF  # bits 7:5 are relevant for channel
+    #     print("milivolt=", milivolt)
+    #     return milivolt
+
+    def extractKBits(num, k, p):
+        # convert number into binary first
+        binary = bin(num)
+
+        # remove first two characters
+        binary = binary[2:]
+
+        end = len(binary) - p
+        start = end - k + 1
+
+        # extract k  bit sub-string
+        kBitSubStr = binary[start: end + 1]
+
+        # convert extracted sub-string into decimal again
+        print(int(kBitSubStr, 2))
+
 
 myWrite_read_Packet_Cls_Obj = Write_Read_Packet_Cls()
 
@@ -182,9 +144,10 @@ print("=========================================================\n\n\n")
 print("==================Write Second try ======================")
 
 myWrite_read_Packet_Cls_Obj.write_encapsulate_frame(0x12, [1, 7, 0, 8, 0, 36, 126, 53])
-#myWrite_read_Packet_Cls_Obj.write_encapsulate_frame(0x12, [1, 126, 0, 126, 0, 36, 126, 53])
 
 print("=========================================================\n\n\n")
+
+# myRead_Packet_Cls2 = Read_Packet_Cls()
 
 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  Read First try  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 
@@ -205,5 +168,77 @@ read_test3_packet: list = (126, 18, 0, 59, 0, 0, 0, 2, 32, 0, 1, 64, 0, 0, 160, 
 myWrite_read_Packet_Cls_Obj.encapsulate_read_frame(read_test3_packet)
 
 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n\n")
+#channels_read2 = bytes(myWrite_read_Packet_Cls_Obj.read_package)
+# channels_read2.append(myWrite_read_Packet_Cls_Obj.read_package[5] & 0xE0)
+# channels_read2.append(myWrite_read_Packet_Cls_Obj.read_package[9] & 0xE0)
+# channels_read2.append(myWrite_read_Packet_Cls_Obj.read_package[13] & 0xE0)
+# channels_read2.append(myWrite_read_Packet_Cls_Obj.read_package[17] & 0xE0)
+# channels_read2.append(myWrite_read_Packet_Cls_Obj.read_package[21] & 0xE0)
+
+#print("channels_read2=", channels_read2)
+
+# measured_voltage_read2=bytes(myWrite_read_Packet_Cls_Obj.read_package)
+# index_i: int = 5
+# sliced_meas_volt1 = bytearray(myWrite_read_Packet_Cls_Obj.read_package[index_i:index_i + 3])
+# sliced_meas_volt2 = bytearray(myWrite_read_Packet_Cls_Obj.read_package[index_i + 4:index_i + 7])
+# sliced_meas_volt3 = bytearray(myWrite_read_Packet_Cls_Obj.read_package[index_i + 8:index_i + 11])
+# sliced_meas_volt4 = bytearray(myWrite_read_Packet_Cls_Obj.read_package[index_i + 12:index_i + 15])
+# sliced_meas_volt5 = bytearray(myWrite_read_Packet_Cls_Obj.read_package[index_i + 16:index_i + 19])
+# print("sliced_meas_volt_1=", sliced_meas_volt1)
+# print("sliced_meas_volt_2=", sliced_meas_volt2)
+# print("sliced_meas_volt_3=", sliced_meas_volt3)
+# print("sliced_meas_volt_4=", sliced_meas_volt4)
+# print("sliced_meas_volt_5=", sliced_meas_volt5)
+
+# measured_voltage_read2.append(myWrite_read_Packet_Cls_Obj.read_package[5:8])
+# measured_voltage_read2.append(myWrite_read_Packet_Cls_Obj.read_package[9:12])
+# measured_voltage_read2.append(myWrite_read_Packet_Cls_Obj.read_package[13:15])
+# measured_voltage_read2.append(myWrite_read_Packet_Cls_Obj.read_package[17:20])
+# measured_voltage_read2.append(myWrite_read_Packet_Cls_Obj.read_package[21:24])
+
+# print("measured_voltage_read2=", measured_voltage_read2)
+# return 0
+# print([myWrite_read_Packet_Cls_Obj.access_bit(myWrite_read_Packet_Cls_Obj.read_package, y) for y in
+#        range(len(myWrite_read_Packet_Cls_Obj.read_package) * 8)])
+#
+# print("sliced_meas_volt1=",
+#       [myWrite_read_Packet_Cls_Obj.access_bit(sliced_meas_volt1, y) for y in range(len(sliced_meas_volt1) * 8)])
+# print("sliced_meas_volt2=",
+#       [myWrite_read_Packet_Cls_Obj.access_bit(sliced_meas_volt2, y) for y in range(len(sliced_meas_volt2) * 8)])
+# print("sliced_meas_volt3=",
+#       [myWrite_read_Packet_Cls_Obj.access_bit(sliced_meas_volt3, y) for y in range(len(sliced_meas_volt3) * 8)])
+# print("sliced_meas_volt4=",
+#       [myWrite_read_Packet_Cls_Obj.access_bit(sliced_meas_volt4, y) for y in range(len(sliced_meas_volt4) * 8)])
+# print("sliced_meas_volt5=",
+#       [myWrite_read_Packet_Cls_Obj.access_bit(sliced_meas_volt5, y) for y in range(len(sliced_meas_volt5) * 8)])
+
+#var = struct.pack(sliced_meas_volt1)
+list_index:list=[5,9,13,17,21]
+meas_result=[]
+print("meas_result",meas_result)
+for index_i in list_index:
+    #print("index i is = ",index_i)
+    int_val1: int = int.from_bytes(myWrite_read_Packet_Cls_Obj.read_package[index_i:index_i + 3], "big")
+    print("index i is = ",index_i,"struct.pack1 new = ", int_val1)
+    meas_result.append(int_val1)
+    print("meas_result after append",meas_result)
+
+#meas_result.append(5)
+#index_i: int = 5
+#int_val1 = int.from_bytes(myWrite_read_Packet_Cls_Obj.read_package[index_i:index_i + 3], "big")
+#int_val2 = int.from_bytes(myWrite_read_Packet_Cls_Obj.read_package[index_i+4:index_i + 7], "big")
+#int_val3 = int.from_bytes(myWrite_read_Packet_Cls_Obj.read_package[index_i+8:index_i + 11], "big")
+#int_val4 = int.from_bytes(myWrite_read_Packet_Cls_Obj.read_package[index_i+12:index_i + 15], "big")
+#int_val5 = int.from_bytes(myWrite_read_Packet_Cls_Obj.read_package[index_i+16:index_i + 19], "big")
+
+#print("struct.pack1 new = ",int_val1)
+#print("struct.pack2 = ",int_val2)
+#print("struct.pack3 = ",int_val3)
+#print("struct.pack4 = ",int_val4)
+#print("struct.pack5 = ",int_val5)
+#print("struct.pack",var)
 
 sys.exit()
+
+
+
