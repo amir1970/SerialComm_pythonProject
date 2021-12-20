d85c0ddf373ef96e581a8a966177549c75c1199e
amir1970 <58373961+amir1970@users.noreply.github.com>
2021-12-20 11:20:07 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 2b137b784c622440db826251b84d50e086bbe9d3.

--- DPVector_Ex2_Section_A.py
+++ DPVector_Ex2_Section_A.py
@@ -1,15 +1,35 @@
 import numpy as np
+#
+# class DPVector(object):
+#
+#     def __init__(self, x: int, y: int) :
+#         self.first_num = x
+#         self.second_num = y
+#
+#     def add_numbers(self):
+#         result = np.add(self.first_num, self.second_num)
+#         print("Adding 2 numbers = ", result)
+#
+#     def __add__(self, other):
+#         return self+other
+import numpy as np
+import operator
 
 class DPVector(object):
+    def __init__(self, *num):
+        self.num = num
 
-    def __init__(self, x: int, y: int) :
-        self.first_num = x
-        self.second_num = y
+    def __repr__(self):
+        return 'DPVector_obj{}'.format(self.num)
 
-    def add_numbers(self):
-        result = np.add(self.first_num, self.second_num)
-        print("Adding 2 numbers = ", result)
+    def __add__(self, other):
+        print("Join list is : ",(self.num+other.num))
+        total = sum(self.num+other.num)
+        print("Total list sum is : ")
+        return total#self.__class__(sum(self.num+other.num))
 
 
-DPVector_obj = DPVector(4,5)
-DPVector_obj.add_numbers()
+a= DPVector(2,5)
+b= DPVector(4,7)
+
+print(a+b)
